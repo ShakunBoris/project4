030218f946ac2a96a6b8f813f468794e1e761f20
ShakunBoris <shakun.boris@gmail.com>
2022-12-11 18:36:07 +0400

    Post Model done, User ManyToMany Follows Done

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from network.models import *
+
+class PostDisplay(admin.ModelAdmin):
+    list_display = ['author', 'text', 'created_at', 'updated_at',]
+    filter_horizontal = ['liked_by',]
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post, PostDisplay)
--- network/models.py
+++ network/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
+    followed_by = models.ManyToManyField('self', symmetrical=False, related_name='is_following')
     pass
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_posts')
